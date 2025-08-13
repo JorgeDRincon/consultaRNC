d186b0f7fac5cbc80967e736456567bb3e41f41e
Jorge D. Rincon <49107349+JorgeDRincon@users.noreply.github.com>
2025-08-12 23:18:08 -0400

    Revert "issue #45: return json for too many requests"

--- routes/api.php
+++ routes/api.php
@@ -9,17 +9,15 @@ Route::get('/user', function (Request $request) {
   return $request->user();
 })->middleware('auth:sanctum');
 
-Route::middleware('throttle:api')->group(function () {
-  Route::get('/search', [RncController::class, 'advancedSearch']);
-  
-  Route::post('/import', [RncImportController::class, 'import']);
-  
-  Route::get('/rnc-import/progress', function () {
-    $progressFile = storage_path('app/import_progress.json');
-    if (file_exists($progressFile)) {
-      $data = json_decode(file_get_contents($progressFile), true);
-      return response()->json($data);
-    }
-    return response()->json(['processed' => 0, 'total' => 0]);
-  });
+Route::get('/search', [RncController::class, 'advancedSearch']);
+
+Route::post('/import', [RncImportController::class, 'import']);
+
+Route::get('/rnc-import/progress', function () {
+  $progressFile = storage_path('app/import_progress.json');
+  if (file_exists($progressFile)) {
+    $data = json_decode(file_get_contents($progressFile), true);
+    return response()->json($data);
+  }
+  return response()->json(['processed' => 0, 'total' => 0]);
 });
