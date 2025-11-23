eb1dba93855606d1c3b51d4bef94c65d88f65237
Jorge Rincon <danielrincon099@gmail.com>
2025-11-23 10:08:40 -0400

    Issue #84: Google analytics tag, corrected

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,32 +20,17 @@
         <!-- Iconos FontAwesome -->
         <script src="https://kit.fontawesome.com/f61d876e0e.js" crossorigin="anonymous"></script>
 
-        <!-- Google Tag Manager -->
-        <script>
-            (
-              function(w,d,s,l,i){
-                w[l]=w[l] || [];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0];
-                j=d.createElement(s);
-                dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              }
-            )(window,document,'script','dataLayer','GTM-WSNGBGX8');
-        </script>
-        <!-- End Google Tag Manager -->
+       <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-LR5PGCZ21P"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LR5PGCZ21P');
+      </script>
     </head>
-    
     <body class="font-sans antialiased bg-white dark:bg-gray-900 transition-colors duration-300">
-        <!-- Google Tag Manager (noscript) -->
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WSNGBGX8"
-            height="0" width="0" style="display:none;visibility:hidden"
-            title="Google Tag Manager (noscript)"
-          ></iframe>
-        </noscript>
         @inertia
     </body>
 </html>
