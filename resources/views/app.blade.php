6399e73ee3ba8683c39ba3d14ef298e1fb4e9cd2
Jorge Rincon <danielrincon099@gmail.com>
2025-11-23 10:01:02 -0400

    Issue #84: Google Analytics tags addedd

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,8 +19,33 @@
 
         <!-- Iconos FontAwesome -->
         <script src="https://kit.fontawesome.com/f61d876e0e.js" crossorigin="anonymous"></script>
+
+        <!-- Google Tag Manager -->
+        <script>
+            (
+              function(w,d,s,l,i){
+                w[l]=w[l] || [];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0];
+                j=d.createElement(s);
+                dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              }
+            )(window,document,'script','dataLayer','GTM-WSNGBGX8');
+        </script>
+        <!-- End Google Tag Manager -->
     </head>
+    
     <body class="font-sans antialiased bg-white dark:bg-gray-900 transition-colors duration-300">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WSNGBGX8"
+            height="0" width="0" style="display:none;visibility:hidden"
+            title="Google Tag Manager (noscript)"
+          ></iframe>
+        </noscript>
         @inertia
     </body>
 </html>
